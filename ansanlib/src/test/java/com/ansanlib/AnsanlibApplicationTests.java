690c9da2bb960d75756c10bb0b7c0e4401e25e53
difmr00 <difmr00@gmail.com>
2024-06-17 15:04:08 +0900

    초기설정 추가

---
+++ ansanlib/src/test/java/com/ansanlib/AnsanlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansanlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsanlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
