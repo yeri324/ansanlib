c20f1a36a25cd0ab4923df6688a10ef08dc4ca70
chanjong <a361212@gmail.com>
2024-06-18 17:17:59 +0900

    조회 수정

--- ansanlib/src/main/java/com/ansanlib/controller/board/FaqController.java
+++ ansanlib/src/main/java/com/ansanlib/controller/board/FaqController.java
@@ -26,6 +26,13 @@ public class FaqController {
 		return "FaqList";
 	}
 	
+//	@PostMapping(value="/faqlist")
+//	public String faqList(Model model) {
+//		List<Faq> faqList = faqService.getFaqList();
+//		model.addAttribute("faqList", faqList);
+//		return "FaqList";
+//	}
+	
 	@GetMapping(value="/faq/new")
 	public String faqNew(Model model) throws Exception{
 		model.addAttribute("FaqFormDto", new FaqFormDto());
@@ -36,6 +43,9 @@ public class FaqController {
 	public String faqNew(FaqFormDto faqFormDto, Model model) throws Exception{
 		try {
 			faqService.saveFaq(faqFormDto);
+			List<Faq> faqList = faqService.getFaqList();
+			model.addAttribute("faqList", faqList);
+			
 		} catch(Exception e) {
 			model.addAttribute("errorMessage", "글 등록 중 에러가 발생하였습니다.");
 			return "FaqForm";
