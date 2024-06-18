8e534fef74785e519976bec24d6e49314fe65ced
chanjong <a361212@gmail.com>
2024-06-18 09:38:25 +0900

    FAQ 생성 기능 추가
    (cherry picked from commit 6bb50f252d778bd1290d20ea092e6f5bf8a541a8)

--- ansanlib/src/main/java/com/ansanlib/service/board/FaqService.java
+++ ansanlib/src/main/java/com/ansanlib/service/board/FaqService.java
@@ -3,6 +3,8 @@ package com.ansanlib.service.board;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ansanlib.dto.board.FaqFormDto;
+import com.ansanlib.entity.Faq;
 import com.ansanlib.repository.board.FaqRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,5 +16,14 @@ public class FaqService {
 	
 	private final FaqRepository faqRepository;
 	
+	public Long saveFaq(FaqFormDto faqFormDto) throws Exception{
+		Faq faq = faqFormDto.createFaq();
+		faqRepository.save(faq);
+		
+		return faq.getId();
+	}
 	
+//	public getListByTitle() throws Exception{
+//		
+//	}
 }
