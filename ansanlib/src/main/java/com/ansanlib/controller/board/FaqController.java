6bb50f252d778bd1290d20ea092e6f5bf8a541a8
chanjong <a361212@gmail.com>
2024-06-18 09:38:25 +0900

    FAQ 생성 기능 추가

--- ansanlib/src/main/java/com/ansanlib/controller/board/FaqController.java
+++ ansanlib/src/main/java/com/ansanlib/controller/board/FaqController.java
@@ -1,10 +1,14 @@
 package com.ansanlib.controller.board;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.ansanlib.dto.board.FaqFormDto;
 import com.ansanlib.service.board.FaqService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -12,5 +16,27 @@ import lombok.RequiredArgsConstructor;
 public class FaqController {
 
 	private final FaqService faqService;
+	
+	@GetMapping(value="/faq")
+	public String faqList() {
+		return "FaqList";
+	}
+	
+	@GetMapping(value="/faq/new")
+	public String faqNew(Model model) throws Exception{
+		model.addAttribute("FaqFormDto", new FaqFormDto());
+		return "FaqForm";
+	}
+	
+	@PostMapping(value="/faq/new")
+	public String faqNew(FaqFormDto faqFormDto, Model model) throws Exception{
+		try {
+			faqService.saveFaq(faqFormDto);
+		} catch(Exception e) {
+			model.addAttribute("errorMessage", "글 등록 중 에러가 발생하였습니다.");
+			return "FaqForm";
+		}
+		return "FaqList";
+	}
 	
 }
