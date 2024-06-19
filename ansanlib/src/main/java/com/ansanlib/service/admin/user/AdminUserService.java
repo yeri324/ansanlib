cc22451c41df54a7287de7df642ab2bd42104670
username <22sss123@naver.com>
2024-06-19 13:35:10 +0900

    커밋

--- ansanlib/src/main/java/com/ansanlib/service/admin/user/AdminUserService.java
+++ ansanlib/src/main/java/com/ansanlib/service/admin/user/AdminUserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ansanlib.dto.admin.user.AdminUserDto;
+import com.ansanlib.entity.LibUser;
 import com.ansanlib.repository.admin.AdminUserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,15 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class AdminUserService {
+public class AdminUserService {public List<LibUser> ListUser(AdminUserDto adminUserDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+public LibUser getUserById(Long id) {
+	// TODO Auto-generated method stub
+	return null;
+}
 //	private final AdminUserRepository adminUserRepository;
 //	
 //	public Page<AdminUserDto> ListUser(AdminUserDto adminUserDto,Pageable pageable){
