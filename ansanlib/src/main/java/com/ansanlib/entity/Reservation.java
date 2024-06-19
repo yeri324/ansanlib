c835f040245a528452151fa13f7d9c3cff3457b4
difmr00 <difmr00@gmail.com>
2024-06-19 15:23:41 +0900

    no message

--- ansanlib/src/main/java/com/ansanlib/entity/Reservation.java
+++ ansanlib/src/main/java/com/ansanlib/entity/Reservation.java
@@ -19,7 +19,7 @@ import lombok.Setter;
 public class Reservation {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	@Column(nullable = false)
