451371215ec1b39843663b21bc0232985562d6d1
jw9932 <kancle@naver.com>
2024-06-20 16:33:59 +0900

    헤더 수정

--- reactlib/src/component/fragments/header/header.js
+++ reactlib/src/component/fragments/header/header.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import './header.css';
+import React, { useState } from 'react';
 
 
-function Header() {
-    return (
-      <header>
-        헤더
-      </header>
-    );
-  }
-  
-  export default Header;
+const Header = ({ useState }) => {
+
+  const menu = [
+    {
+      menu1: { title: "소개" },
+      subs: [
+        { title: "연혁" },
+        { title: "조직도" },
+        { title: "도서관" }
+      ]
+    }
+  ];
+
+  return (
+    <div class="header_menu">
+      {menu.map((menuItem, index) => (
+        <div key={index}>
+          {menuItem.menu1.title}
+          <div className="sub_menu">
+            {menuItem.subs.map((sub, subIndex) => (
+              <div key={subIndex}>{sub.title}</div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
+export default Header;
