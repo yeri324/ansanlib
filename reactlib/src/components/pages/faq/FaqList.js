775ec6d99cb21e51ff134e31727c05cff15d4074
chanjong <a361212@gmail.com>
2024-06-20 18:42:59 +0900

    병합전 커밋

--- reactlib/src/components/pages/faq/FaqList.js
+++ reactlib/src/components/pages/faq/FaqList.js
@@ -1,10 +1,10 @@
 import './FaqList.css';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function FaqList() {
-    const [faqList, setFaqList] = useState()
+    const [faqList, setFaqList] = useState();
 
     useEffect(() => {
         getDataset();
@@ -23,29 +23,29 @@ function FaqList() {
 
     return (
         <div>
-            {faqList && faqList.map((item, index) => (
-                <div key={index} className="slide">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>번호</th>
-                                <th>제목</th>
-                                <th>작성시간</th>
-                               
-                            </tr>
-                        </thead>
+            <table>
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>작성시간</th>
+
+                    </tr>
+                </thead>
+                {faqList && faqList.map((item, index) => (
+                    <div key={index} className="slide">
+
                         <div >
                             <tr>
-                            <th><a>{item.id}</a></th>
-                            <th>{item.title}</th>
-                            <th>{item.regTime}</th>
+                                <th>{item.id}</th>
+                                <th><Link to={`/faq/detail/${item.id}`}>{item.title}</Link></th>
+                                <th>{item.regTime}</th>
                             </tr>
                         </div>
-                    </table>
-                </div>
 
-            ))}
-
+                    </div>
+                ))}
+            </table>
         </div>
     );
 };
