2c4811f08798f1834b4e97faf65ac396bfef89be
jw9932 <kancle@naver.com>
2024-06-20 18:14:50 +0900

    헤더

--- reactlib/src/App.js
+++ reactlib/src/App.js
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
 import { Routes, Route } from 'react-router-dom';
 import FaqList from './components/pages/faq/FaqList';
 import FaqForm from './components/pages/faq/FaqForm';
 import FaqDetailForm from './components/pages/faq/FaqDetailForm';
+import Header from './components/fragments/header/header';
 
 function App() {
 
   return (
     <div>
       <Routes>
+        <Route path="/fragments/header" element={<Header />} />
         <Route path="/faq/list" element={<FaqList />} />
         <Route path="/faq/new" element={<FaqForm />} />
         <Route path="/faq/detail/:id" element={<FaqDetailForm />} />
--- reactlib/src/components/fragments/header/header.js
+++ reactlib/src/components/fragments/header/header.js
@@ -3,30 +3,60 @@ import './header.css';
 
 
 const Header = () => {
-
-  const menu = [
+  const menus = [
     {
-      menu1: { title: "소개" },
+      menu: { title: "소개" },
       subs: [
         { title: "연혁" },
         { title: "조직도" },
         { title: "도서관" }
+      ],
+      menu: { title: "검색"},
+      subs: [
+        { title: "통합검색" },
+        { title: "상세검색" },
+        { title: "추천도서" },
+        { title: "신간도서" }
+      ],
+      menu: { title: "책읽는 안산"},
+      subs: [
+        { title: "이달의 도서" },
+        { title: "인기도서" },
+        { title: "추천게시판" }
+      ],
+      menu: { title: "게시판"},
+      subs: [
+        { title: "공지사항" },
+        { title: "FAQ" }
+      ],
+      menu: { title: "나의 도서관"},
+      subs: [
+        { title: "나만의 책장" },
+        { title: "예약/대출현황" },
+        { title: "정보수정" },
+        { title: "희망도서" }
       ]
     }
   ];
 
   return (
-    <div class="header_menu">
-      {menu.map((menuItem, index) => (
-        <div key={index}>
-          {menuItem.menu1.title}
-          <div className="sub_menu">
-            {menuItem.subs.map((sub, subIndex) => (
-              <div key={subIndex}>{sub.title}</div>
-            ))}
+    <div className="full_header">
+      <div className="top_login">
+        {/* <a className="enter" href="">로그인</a>
+        <a className="join" href="">회원가입</a> */}
+      </div>
+      <div className="header_menu">
+        {menus.map((menuItem, index) => (
+          <div key={index}>
+            <div>{menuItem.menu.title}</div>
+            <div className="sub_menu">
+              {menuItem.subs.map((sub, subIndex) => (
+                <div key={subIndex}>{sub.title}</div>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
