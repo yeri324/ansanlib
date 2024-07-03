8adee464cf597f638e33cfe227dfc4874e2d7960
cksdid1363 <cksdid1363@gmail.com>
2024-07-03 15:09:42 +0900

    대출 목록조회(로그인 상태에서만)

--- reactlib/src/components/pages/loanStatus/LoanStatusList.js
+++ reactlib/src/components/pages/loanStatus/LoanStatusList.js
@@ -1,29 +1,45 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
-import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const LoanStatusList = ()=>{
-    const {userId} = useParams();
+    const navigate = useNavigate();
+    
+    const [userId, setUserId] = useState(null);
+    const [userName, setUserName] = useState("");
     const [loanStatuses, setLoanStatuses] = useState([]);
     const [isErrored, setErrored] =useState(false);
 
     useEffect(()=>{
+        const memberData = JSON.parse(sessionStorage.getItem("member") ?? "null");
+        if(memberData?.userId){
+            setUserId(memberData?.userId);
+            setUserName(memberData?.name);
+        } else {
+            setErrored(true);
+            alert("로그인이 되어있지 않습니다.");
+            navigate("/login");
+        }
+    },[]);
+
         const LoanStatuses = async()=>{
-            try{
-                const response = await axios.get(`/api/loanstatus/get/by-user/${userId}`);
-                setLoanStatuses(response.data);
-            } catch(error){
-                setErrored(true);
-                console.error('대출상태를 가져오는 중에 오류가 발생했습니다.',error);
+            if(userId) {
+                try{
+                    const response = await axios.get(`/api/loanstatus/get/by-user/${userId}`);
+                    setLoanStatuses(response.data);
+                } catch(error){
+                    setErrored(true);
+                    console.error('대출상태를 가져오는 중에 오류가 발생했습니다.',error);
+                }
             }
+            
         };
 
-        LoanStatuses();
-    },[userId]);
+        useEffect(() => { LoanStatuses();}, [userId]);
 
     return (
         <div>
-            <h2> 대출 상태 </h2>
+            <h2> {userName}의 대출 상태 </h2>
             {loanStatuses.length > 0 ?(
                  <ul>
                  {loanStatuses.map(status=>(
