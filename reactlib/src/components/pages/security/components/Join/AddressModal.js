8ad5cccbc109f06c58d58fa0fb098e6248b5e9d0
difmr00 <difmr00@gmail.com>
2024-07-11 22:09:37 +0900

    회원가입 유효성 검사(주소 아직 못했음)

---
+++ reactlib/src/components/pages/security/components/Join/AddressModal.js
@@ -0,0 +1,9 @@
+import ReactModal from "react-modal";
+import DaumPostcodeEmbed from "react-daum-postcode";
+
+
+const AddressModal = ({isOpen, onClose, onSelect}) => {
+
+}
+
+export default AddressModal;
--- reactlib/src/components/pages/security/components/Join/FormJoin.js
+++ reactlib/src/components/pages/security/components/Join/FormJoin.js
@@ -1,10 +1,12 @@
-import React, { useState,useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { LoginContext } from '../../contexts/LoginContextProvider';
 import LoginContextConsumer from '../../contexts/LoginContextConsumer';
+import DaumPostcode from 'react-daum-postcode';
 import { getData } from '../../apis/auth';
+import './FormJoin.css'
 
-const FormJoin = ({ join,}) => {
-    const [errors, setErrors] = useState({});
+const FormJoin = ({ join, }) => {
+    const [errors, setErrors] = useState('');
     const [formData, setFormData] = useState({
         loginid: '',
         password: '',
@@ -12,16 +14,20 @@ const FormJoin = ({ join,}) => {
         name: '',
         phone: '',
         email: '',
+        email1: '',
+        email2: '',
         address: '',
         address2: '',
         gender: 'male',
         sms: 'yes',
     });
     const [inputDisable, setInputDisable] = useState({
-        isloginid : false,
-        isEmail : false,
+        isloginid: false,
+        isEmail: false,
     })
-
+    const [passwordValid, setPasswordValid] = useState(true);
+    const [passwordMatch, setPasswordMatch] = useState(true);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     //setFormData
     const handleChange = (e) => {
@@ -32,81 +38,95 @@ const FormJoin = ({ join,}) => {
             setFormData({ ...formData, phone: fPhone });
         }
 
-        else{
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    }
+        else {
+            setFormData({
+                ...formData,
+                [name]: value,
+            });
+        }
     };
 
     //회원가입 메소드로 데이터 전송
     const onJoin = (e) => {
         e.preventDefault();
-         onValidate();
-            join(formData);
+        onValidate();
     };
 
- 
-    //비밀번호 확인
-    useEffect(()=>{
-        if(password===password2){}
-    },[formData.password,formData.password2])
-
     //아이디 중복 체크
-    const onCheckId = async ()=>{
-        if(formData.loginid==null ||formData.loginid==""){
+    const onCheckId = async () => {
+        if (formData.loginid == null || formData.loginid == "") {
             alert("아이디를 입력해주세요.");
             return;
         }
-        const data = {loginid: formData.loginid}
+        const data = { loginid: formData.loginid }
         try {
-            await getData(data).then((response)=>{ 
-              if( response.status == 200 ) { 
-              console.log(`사용 가능한 아이디!`);
-              alert("사용 가능한 아이디입니다.");
-              setInputDisable({ ...inputDisable, isloginid: true })
-            }})
-             
-          } catch (error) {
-            if(error.response.status == 409 ){ 
-              console.log(`사용중인 아이디!`);
-              alert("사용 할 수 없는 아이디입니다.")
+            await getData(data).then((response) => {
+                if (response.status == 200) {
+                    console.log(`사용 가능한 아이디!`);
+                    alert("사용 가능한 아이디입니다.");
+                    setInputDisable({ ...inputDisable, isloginid: true })
+                }
+            })
+
+        } catch (error) {
+            if (error.response.status == 409) {
+                console.log(`사용중인 아이디!`);
+                alert("사용 할 수 없는 아이디입니다.")
             }
             else {
-              console.log(`오류!`);
-              alert("조회 중 오류가 발생하였습니다.")
+                console.log(`오류!`);
+                alert("조회 중 오류가 발생하였습니다.")
             }
-          }
+        }
     };
 
-
     //이메일 중복 체크
-    const onCheckEmail = async ()=>{
-        if(formData.email==null ||formData.email==""){
+    const onCheckEmail = async () => {
+        if (formData.email == null || formData.email == "") {
             alert("이메일을 입력해주세요.");
             return;
         }
-        const data = {email: formData.email}      
+        const data = { email: formData.email }
         try {
-            await getData(data).then((response)=>{ 
-              if( response.status == 200 ) { 
-              console.log(`사용 가능한 이메일!`);
-              alert("사용 가능한 이메일입니다.");
-              setInputDisable({ ...inputDisable, isEmail: true })
-            }})
-             
-          } catch (error) {
-            if(error.response.status == 409 ){ 
-              console.log(`사용중인 이메일!`);
-              alert("사용 할 수 없는 이메일입니다.")
+            await getData(data).then((response) => {
+                if (response.status == 200) {
+                    console.log(`사용 가능한 이메일!`);
+                    alert("사용 가능한 이메일입니다.");
+                    setInputDisable({ ...inputDisable, isEmail: true })
+                }
+            })
+
+        } catch (error) {
+            if (error.response.status == 409) {
+                console.log(`사용중인 이메일!`);
+                alert("사용 할 수 없는 이메일입니다.")
             }
             else {
-              console.log(`오류!`);
-              alert("조회 중 오류가 발생하였습니다.")
+                console.log(`오류!`);
+                alert("조회 중 오류가 발생하였습니다.")
             }
-          }
+        }
     };
+
+    //비밀번호 유효성
+    const onCheckPassword = (password) => {
+        const regex = /^(?=.*[A-Za-z])(?=.*\d|(?=.*\W)).{8,16}$/;
+        return regex.test(password);
+    }
+
+    //비밀번호 확인
+    const onMatchPassword = () => {
+        setPasswordMatch(formData.password === formData.password2);
+    }
+
+    //비밀번호 유효성검사 + 확인
+    useEffect(() => {
+
+        const isValid = onCheckPassword(formData.password);
+        setPasswordValid(isValid);
+        onMatchPassword();
+
+    }, [formData.password, formData.password2])
 
     //핸드폰 번호 포매팅
     const formatPhone = (str) => {
@@ -131,11 +151,46 @@ const FormJoin = ({ join,}) => {
         }
     };
 
+    //주소 모달창부터~
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleAddressComplete = () => {
+        setIsModalOpen(false);
+    };
+
+
+    //유효성 검사 마무리
+    const onValidate = () => {
+        if (!inputDisable.isloginid) {
+            alert('아이디 중복 체크를 완료해주세요.');
+            return
+        }
+        if (!inputDisable.isEmail) {
+            alert('이메일 중복 체크를 완료해주세요.');
+            return
+        }
+        if (formData.name.trim() === '' || formData.name == null) {
+            alert('이름을 입력해주세요.');
+            return
+        }
+        if (formData.phone.trim() === '' || formData.phone == null) {
+            alert('핸드폰번호를 입력해주세요.');
+            return
+        }
+        if (formData.address.trim() === '' || formData.address == null) {
+            alert('주소를 입력해주세요.');
+            return
+        }
+
+    }
+
     useEffect(()=>{
+        setFormData({...formData, email : formData.email1 + '@' + formData.email2})
+    },[formData.email1, formData.email2])
 
-    },[formData])
 
-   
     return (
         <div className="form">
             <h2 className="login-title">Join</h2>
@@ -152,7 +207,7 @@ const FormJoin = ({ join,}) => {
                         onChange={handleChange}
                         disabled={inputDisable.isloginid}
                     />
-                    <button type='button' onClick={onCheckId}>아이디체크</button>
+                    <button type='button' disabled={inputDisable.isloginid} onClick={onCheckId}>아이디체크</button>
                 </div>
                 <div>
                     <label htmlFor="password">비밀번호</label>
@@ -161,9 +216,12 @@ const FormJoin = ({ join,}) => {
                         type="password"
                         placeholder="비밀번호를 입력해주세요!"
                         name="password"
+                        minLength={8}
+                        maxLength={16}
                         value={formData.password}
                         onChange={handleChange}
                     />
+                    {!passwordValid && <p style={{ color: 'red' }}>8자 이상 16자 이하로 영문자, 숫자, 특수문자 중 두 가지 이상 포함해야 합니다.</p>}
                 </div>
                 <div>
                     <label htmlFor="password2">비밀번호확인</label>
@@ -172,9 +230,12 @@ const FormJoin = ({ join,}) => {
                         type="password"
                         placeholder="작성한 비밀번호를 입력해주세요!"
                         name="password2"
+                        minLength={8}
+                        maxLength={16}
                         value={formData.password2}
                         onChange={handleChange}
                     />
+                    {!passwordMatch && <p style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</p>}
                 </div>
                 <div>
                     <label htmlFor="name">이름</label>
@@ -194,6 +255,7 @@ const FormJoin = ({ join,}) => {
                         type="text"
                         placeholder="핸드폰번호를 입력해주세요!"
                         name="phone"
+                        maxLength={15}
                         value={formData.phone}
                         onChange={handleChange}
                     />
@@ -202,25 +264,39 @@ const FormJoin = ({ join,}) => {
                     <label htmlFor="email">Email</label>
                     <input
                         id="email"
-                        type="email"
+                        type="text"
                         placeholder="Email을 입력해주세요!"
-                        name="email"
-                        value={formData.email}
+                        name="email1"
+                        value={formData.email1}
                         onChange={handleChange}
                         disabled={inputDisable.isEmail}
-                    />
-                      <button type='button' onClick={onCheckEmail}>이메일체크</button>
+                    />@
+                   <select value={formData.email2} name="email2" disabled={inputDisable.isEmail} onChange={handleChange}>
+                    <option value='naver.com'>naver.com</option>
+                    <option value="gmail.com">gmail.com</option>
+                    <option value='daum.net'>daum.net</option>
+                    <option value='hanmail.net'>hanmail.net</option>
+                    <option value='nate.com'>nate.com</option>
+
+                </select>
+                    <button type='button' onClick={onCheckEmail}>이메일체크</button>
                 </div>
                 <div>
                     <label htmlFor="address">주소</label>
-                    <input
+                    {/* <input
                         id="address"
                         type="address"
                         placeholder="주소를 입력해주세요!"
                         name="address"
                         value={formData.address}
                         onChange={handleChange}
-                    />
+                    /> */}
+                    <button type='button' onClick={() => setIsModalOpen(true)}>주소 검색</button>
+                    {isModalOpen && (
+                        <DaumPostcode
+                            onComplete={handleAddressComplete}
+                            autoClose = {true}
+                            animation />)}
                 </div>
                 <div>
                     <label htmlFor="address2">상세주소</label>
@@ -233,19 +309,19 @@ const FormJoin = ({ join,}) => {
                         onChange={handleChange}
                     />
                 </div>
-                <div>
-                    <label >성별</label>
-                    <input type="radio" name="gender" id="male" value="male"   checked={formData.gender === 'male'} onChange={handleChange}/>
-                    <label htmlFor="male">남성</label>
-                    <input type="radio" name="gender" id="female" value="female"  checked={formData.gender === 'female'} onChange={handleChange}/>
-                    <label htmlFor="female">여성</label>
+                <div class='join-radio'>
+                    <label>성별</label>
+                    <input type="radio" name="gender" id="male" value="male" checked={formData.gender === 'male'} onChange={handleChange} />
+                    <label htmlFor="male" class='check-label'><span>남성</span></label>
+                    <input type="radio" name="gender" id="female" value="female" checked={formData.gender === 'female'} onChange={handleChange} />
+                    <label htmlFor="female" class='check-label'><span>여성</span></label>
                 </div>
-                <div>
-                    <label htmlFor="address">SMS수신동의</label>
-                    <input type="radio" name="sms" id="sms_yes" value="yes"  checked={formData.sms === 'yes'} onChange={handleChange}/>
-                    <label htmlFor="male">동의</label>
+                <div class='join-radio'>
+                    <label htmlFor="address">SMS수신</label>
+                    <input type="radio" name="sms" id="sms_yes" value="yes" checked={formData.sms === 'yes'} onChange={handleChange} />
+                    <label htmlFor="sms_yes" class='check-label'><span>동의</span></label>
                     <input type="radio" name="sms" id="sms_no" value="no" checked={formData.sms === 'no'} onChange={handleChange} />
-                    <label htmlFor="female">비동의</label>
+                    <label htmlFor="sms_no" class='check-label'><span>비동의</span></label>
                 </div>
                 <button className="btn btn--form btn-login" type="submit">
                     Join
