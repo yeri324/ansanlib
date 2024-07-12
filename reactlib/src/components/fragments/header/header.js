2369bfc1ab2840bba8da8ced3ca12fbfaee8412f
jw9932 <kancle@naver.com>
2024-07-12 17:52:32 +0900

    안산은 이게 일상이야

--- reactlib/src/components/fragments/header/header.js
+++ reactlib/src/components/fragments/header/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './header.css';
 import center_logo from '../../images/logo/center_logo.png';
 
@@ -7,42 +8,42 @@ const Header = () => {
     {
       menu: { title: "소개" },
       subs: [
-        { title: "연혁" },
-        { title: "조직도" },
-        { title: "도서관" }
+        { title: "연혁", link: "/intro/history" },
+        { title: "조직도", link: "/intro/organization" },
+        { title: "도서관", link: "/intro/library" }
       ]
     },
     {
       menu: { title: "검색" },
       subs: [
-        { title: "통합검색" },
-        { title: "상세검색" },
-        { title: "추천도서" },
-        { title: "신간도서" }
+        { title: "통합검색", link: "/search/integrated" },
+        { title: "상세검색", link: "/search/advanced" },
+        { title: "추천도서", link: "/search/recommended" },
+        { title: "신간도서", link: "/search/new" }
       ]
     },
     {
       menu: { title: "책읽는 안산" },
       subs: [
-        { title: "이달의 도서" },
-        { title: "인기도서" },
-        { title: "추천게시판" }
+        { title: "이달의 도서", link: "/reading/monthly" },
+        { title: "인기도서", link: "/reading/popular" },
+        { title: "추천게시판", link: "/reading/recommendations" }
       ]
     },
     {
       menu: { title: "게시판" },
       subs: [
-        { title: "공지사항" },
-        { title: "FAQ" }
+        { title: "공지사항", link: "/board/announcements" },
+        { title: "FAQ", link: "/board/faq" }
       ]
     },
     {
       menu: { title: "나의 도서관" },
       subs: [
-        { title: "나만의 책장" },
-        { title: "예약/대출현황" },
-        { title: "정보수정" },
-        { title: "희망도서" }
+        { title: "나만의 책장", link: "/mylibrary/bookshelf" },
+        { title: "예약/대출현황", link: "/mylibrary/status" },
+        { title: "정보수정", link: "/mylibrary/edit" },
+        { title: "희망도서", link: "/mylibrary/wishlist" }
       ]
     }
   ];
@@ -56,15 +57,19 @@ const Header = () => {
         </div>
         <div id="header_menu">
           <div id="top_logo">
-            <img src={center_logo} alt="Center Logo" />
+            <Link to="/home">
+              <img src={center_logo} alt="Center Logo" />
+            </Link>
           </div>
-          <div id="top_menu">
+          <div id="top_menu" className="header_links">
             {menus.map((menuItem, index) => (
               <div key={index}>
                 <div>{menuItem.menu.title}</div>
                 <div className="sub_menu">
                   {menuItem.subs.map((sub, subIndex) => (
-                    <div key={subIndex}>{sub.title}</div>
+                    <div key={subIndex}>
+                      <Link to={sub.link}>{sub.title}</Link>
+                    </div>
                   ))}
                 </div>
               </div>
