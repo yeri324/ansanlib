124b8edd167a3e002018b4fcad514962e6d084ab
difmr00 <difmr00@gmail.com>
2024-07-16 08:00:03 +0900

    팝업관리페이지 함 만들어보까

---
+++ ansanlib/src/main/java/com/ansanlib/admin/repository/PopupRepository.java
@@ -0,0 +1,9 @@
+package com.ansanlib.admin.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ansanlib.entity.Popup;
+
+public interface PopupRepository extends JpaRepository<Popup, Long> {
+
+}
---
+++ ansanlib/src/main/java/com/ansanlib/entity/Popup.java
@@ -0,0 +1,29 @@
+package com.ansanlib.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Popup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    
+    private String imgUrl;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+
+}
