851695fa14d8bf06bc76570b3617edd1339cad65
username <22sss123@naver.com>
2024-07-24 17:09:09 +0900

    투데이방문수

--- ansanlib/src/main/java/com/ansanlib/visit/service/VisitService.java
+++ ansanlib/src/main/java/com/ansanlib/visit/service/VisitService.java
@@ -9,5 +9,6 @@ public interface VisitService {
 	
     Visit recordVisit(VisitDto visitDto);
     List<Visit> getAllVisits();
+    long getVisitCountToday(); // 오늘의 방문자 수를 계산하는 메서드
     long getVisitCount();
 }
