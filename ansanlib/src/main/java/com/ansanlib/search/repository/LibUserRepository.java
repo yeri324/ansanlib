37b4ff4399260128fc44f4e7813ddc0a03355c62
jw9932 <kancle@naver.com>
2024-07-25 16:32:28 +0900

    no message

--- ansanlib/src/main/java/com/ansanlib/search/repository/LibUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ansanlib.search.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ansanlib.entity.LibUser;
-
-public interface LibUserRepository extends JpaRepository<LibUser, Long> {
-
-}
