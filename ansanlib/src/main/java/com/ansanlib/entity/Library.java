9c01b4effdd9b6f816888188c23b712ec8f86a61
username <22sss123@naver.com>
2024-07-31 16:59:54 +0900

    라이부러리변경

--- ansanlib/src/main/java/com/ansanlib/entity/Library.java
+++ ansanlib/src/main/java/com/ansanlib/entity/Library.java
@@ -7,12 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Library extends BaseEntity {
 
 	@Id
