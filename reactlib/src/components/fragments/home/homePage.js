8b5f698ebeea0090da6abc83c71f3eb9e338e75f
fdsa1209 <fdsa1209@gmail.com>
2024-07-31 11:01:13 +0900

    메인페이지 캘린더 휴관도서관 출력

--- reactlib/src/components/fragments/home/homePage.js
+++ reactlib/src/components/fragments/home/homePage.js
@@ -6,10 +6,14 @@ import BookSearch from './booksearch';
 import Trends from './Trends';
 import Notice from './notice';
 import New from './new';
-import TodoCalendar from './calendar';
+import HolidayDetail from '../../pages/admin/modal/HolidayDetail';
+import Calendar from 'react-calendar';
 import "react-calendar/dist/Calendar.css";
 import Kakao from '../../map/mapForm';
 import Popup from './popup';
+import useAuth, { LOGIN_STATUS, ROLES } from '../../hooks/useAuth';
+import Auth from '../../helpers/Auth';
+import RedirectLogin from '../../helpers/RedirectLogin';
 // import KeywordCloud from './KeywordCloud';
 // import KeywordCloud_bookId from './KeywordCloud_bookId';
 // import BookCloud from './bookCloud';
@@ -20,7 +24,12 @@ import axios from 'axios';
 
 const HomePage = () => {
 
+  const { axios } = useAuth();
   const [popList, setPopList] = useState([])
+  const [date, setDate] = useState(new Date());
+  const [showDetail, setShowDetail] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedHolidays, setSelectedHolidays] = useState([]);
 
   useEffect(() => {
     getPopList()
@@ -36,6 +45,44 @@ const HomePage = () => {
       setPopList(res.data);
     })
   }
+
+  
+
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+    fetchSelectedDateHolidays(date);
+    setShowDetail(true);
+  };
+
+  const fetchSelectedDateHolidays = (date) => {
+    const adjustDate = (date, days) => {
+      const result = new Date(date);
+      result.setDate(result.getDate() + days);
+      return result;
+    };
+
+    const nextDate = adjustDate(date, 1);
+
+    console.log("Fetching holidays for selected date:", nextDate);
+
+    axios({
+      url: '/api/admin/holiday/list',
+      method: 'get',
+      params: {
+        date: nextDate.toISOString().split('T')[0]
+      }
+    }).then((response) => {
+      console.log("Fetched holidays for selected date:", response.data);
+      setSelectedHolidays(response.data.result);
+    }).catch((error) => {
+      console.error("Error fetching holidays for selected date:", error);
+      setSelectedHolidays([]);
+    });
+  };
+
+  const handleCloseModal = () => {
+    setShowDetail(false);
+  };
 
   return (
     <>
@@ -59,7 +106,16 @@ const HomePage = () => {
                   <Notice />
                 </div>
                 <div className="monthly-plan">
-                  <TodoCalendar />
+                <Calendar
+                locale="en-US"
+                onClickDay={handleDateClick}
+                value={date}
+              />
+              <HolidayDetail
+                show={showDetail}
+                handleClose={handleCloseModal}
+                holidays={selectedHolidays}
+              />
                 </div>
                 <div className="login-box">
                   <div className="new-h2"><h2>신규도서</h2></div>
@@ -91,4 +147,13 @@ const HomePage = () => {
   );
 }
 
-export default HomePage;
+export default function () {
+  return (
+    <>
+      <RedirectLogin />
+      <Auth loginStatus={LOGIN_STATUS.LOGGED_IN} roles={ROLES.ADMIN}>
+        <HomePage />
+      </Auth>
+    </>
+  );
+}
